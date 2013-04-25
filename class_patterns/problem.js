7fffc20d4e23ce59ba00e6465095a3325f5b5b2b
Whoaa512 <whoaa512@gmail.com>
2013-04-25 16:30:18 -0700

    Added class_patterns files & folders

---
+++ class_patterns/problem.js
@@ -0,0 +1,24 @@
+var makePhone = function(phoneNumber){
+  var result = {};
+  result.phoneNumber = phoneNumber;
+  result.send = function(recipientPhoneNumber, message){
+    console.log('sending the message "'+ message +'" to ' + recipientPhoneNumber);
+  }
+  return result;
+};
+
+var makeSmartPhone = function(phoneNumber, phoneEmail){
+  result = makePhone(phoneNumber);
+  result.phoneEmail = phoneEmail;
+  var oldSend = result.send;
+  result.send = function(recipientPhoneNumberOrEmail, message){
+    if(typeof recipientPhoneNumberOrEmail === 'number'){
+      var recipientPhoneNumber = recipientPhoneNumberOrEmail;
+      oldSend(recipientPhoneNumber, message);
+    } else {
+      var recipientEmail = recipientPhoneNumberOrEmail;
+      console.log('sending the message "'+ message +'" to ' + recipientEmail);
+    }
+  }
+  return result;
+};
