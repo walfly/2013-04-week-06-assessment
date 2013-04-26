0cf3b51e4c7ff787f2be63dd9c9cb886dee13342
Larry Davis <lazdnet@gmail.com>
2013-04-26 10:59:23 -0700

    Use chai's expect method

--- recursion/spec/problemSpec.js
+++ recursion/spec/problemSpec.js
@@ -2,6 +2,6 @@ describe('recursiveNumPrinter', function() {
   it("should print a string of numbers from a nested array(click to view test)", function() {
     var deeplyNestedArray = [1,2,33,[99,22,[88,[100],22,334],9000],3,9001,[315,68,88,[290],11],57];
     var expectedString = '1,2,33,99,22,88,100,22,334,9000,3,9001,315,68,88,290,11,57';
-    expect(recursiveNumPrinter(deeplyNestedArray)).toEqual(expectedString);
+    expect(recursiveNumPrinter(deeplyNestedArray)).to.equal(expectedString);
   });
 });
