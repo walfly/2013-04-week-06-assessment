5f959acaed5b0a344fd91a3a88bf9e94cf7396da
Larry Davis <lazdnet@gmail.com>
2013-04-26 10:13:28 -0700

    Drop the JSONP URL

--- ajax/problem.js
+++ ajax/problem.js
@@ -1,5 +1,6 @@
 /* Provided URLs */
-var firstUrl  = 'http://nodejs.org/api/http.json',
-    secondUrl  = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http%3A%2F%2Fgoo.gl%2F183wm&key=AIzaSyCFj15TpkchL4OUhLD1Q2zgxQnMb7v3XaM';
+var firstUrl = 'http://nodejs.org/api/http.json',
+    secondUrl = 'http://nodejs.org/api/process.json',
+    thirdUrl = 'http://nodejs.org/api/fs.json';
 
 /* Write your solution below */
