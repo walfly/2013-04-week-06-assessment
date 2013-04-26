f9352e946357b98f44bf1cd1291a81b40d744b3e
Whoaa512 <whoaa512@gmail.com>
2013-04-25 23:44:44 -0700

    Comments & newline in ajax file

--- ajax/problem.js
+++ ajax/problem.js
@@ -1,3 +1,6 @@
+/* Provided URLs */
 var firstUrl  = 'http://nodejs.org/api/http.json',
     secondUrl = 'http://nodejs.org/api/index.html',
     thirdUrl  = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http%3A%2F%2Fgoo.gl%2F183wm&key=AIzaSyCFj15TpkchL4OUhLD1Q2zgxQnMb7v3XaM';
+
+/* Write your solution below */
