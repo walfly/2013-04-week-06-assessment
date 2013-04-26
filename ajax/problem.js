66c6955c88b5d6c08fef7fe18db958192beb212c
Larry Davis <lazdnet@gmail.com>
2013-04-26 10:10:12 -0700

    Removed secondUrl (not cross-domain compatible)

--- ajax/problem.js
+++ ajax/problem.js
@@ -1,6 +1,5 @@
 /* Provided URLs */
 var firstUrl  = 'http://nodejs.org/api/http.json',
-    secondUrl = 'http://nodejs.org/api/index.html',
-    thirdUrl  = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http%3A%2F%2Fgoo.gl%2F183wm&key=AIzaSyCFj15TpkchL4OUhLD1Q2zgxQnMb7v3XaM';
+    secondUrl  = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl=http%3A%2F%2Fgoo.gl%2F183wm&key=AIzaSyCFj15TpkchL4OUhLD1Q2zgxQnMb7v3XaM';
 
 /* Write your solution below */
